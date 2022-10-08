cd1a3884aa5311b38dc533b8db42b1bfdbd88092
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-10-08 02:38:42 -0400

    new chapters

--- Topics/Processing strings/The lucky ticket/src/Main.kt
+++ Topics/Processing strings/The lucky ticket/src/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val list = readLine()!!.map { it.digitToInt() }
-    if ((list.subList(0,3).sum()) == (list.subList(3,6).sum())) {
+    val list = readln().map { it.digitToInt() }
+    if (list.subList(0, 3).sum() == list.subList(3, 6).sum()) {
         println("Lucky")
     } else {
         println("Regular")
